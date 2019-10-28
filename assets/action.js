dd62c682e30f72479dd16af9a85f45841504ced7
James Buteera <butera1983@outlook.com>
2019-10-27 21:55:44 -0400

    addded small javascript,small css as well as html being bigger part.Tomorrow I will add inpute and save to local storage

---
+++ assets/action.js
@@ -0,0 +1,39 @@
+
+
+var inputschedule = document.querySelector(".schedule");
+var inputj = document.querySelector(".j");
+
+
+inputej.addEventListener("click",function(){
+    localStorage.setItem("",j);
+})
+
+
+
+
+
+
+
+
+
+var counter = document.querySelector("#counter");
+var addButton = document.querySelector("#add");
+var subtractButton = document.querySelector("#subtract");
+
+var count = localStorage.getItem("count");
+
+counter.textContent = count;
+
+addButton.addEventListener("click", function() {
+  count++;
+  counter.textContent = count;
+
+  localStorage.setItem("count", count);
+});
+
+subtractButton.addEventListener("click", function() {
+  count--;
+  counter.textContent = count;
+
+  localStorage.setItem("count", count);
+});
